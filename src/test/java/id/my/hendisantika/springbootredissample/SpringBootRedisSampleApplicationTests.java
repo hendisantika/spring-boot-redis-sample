1b9d97f8511cc700061e43a05f0bf3ce1a627fc3
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 09:29:39 +0700

    feat: Add Redis Testcontainers🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/springbootredissample/SpringBootRedisSampleApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootredissample/SpringBootRedisSampleApplicationTests.java
@@ -1,13 +1,56 @@
 package id.my.hendisantika.springbootredissample;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootRedisSampleApplicationTests {
 
+    private static final String REDIS_IMAGE_NAME = "redis:7.0-alpine";
+    private static final int REDIS_PORT = 6379;
+    @Container
+    private static final GenericContainer<?> redis =
+            new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE_NAME))
+                    .withExposedPorts(REDIS_PORT);
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @BeforeAll
+    static void beforeAll() {
+        // No need to start the container here, @Container will handle it
+    }
+
+    @AfterAll
+    static void afterAll() {
+        // No need to stop the container here, @Container will handle it
+    }
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+    }
+
     @Test
-    void contextLoads() {
+    void testSaveKeyValueAndGetValue() {
+        final String key = "testKey", value = "testValue";
+
+        redisTemplate.opsForValue().set(key, value);
+
+        assertThat(redisTemplate.opsForValue().get(key)).isEqualTo(value);
     }
 
 }
