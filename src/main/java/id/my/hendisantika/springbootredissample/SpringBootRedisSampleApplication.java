c496ef67fec94b2bd05f9f0330b02a1ddb7680ce
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 07:47:44 +0700

    feat: Add redisTemplate🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootredissample/SpringBootRedisSampleApplication.java
+++ src/main/java/id/my/hendisantika/springbootredissample/SpringBootRedisSampleApplication.java
@@ -2,7 +2,12 @@ package id.my.hendisantika.springbootredissample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
+@EnableCaching // Enables Spring’s annotation-driven cache management capability
 @SpringBootApplication
 public class SpringBootRedisSampleApplication {
 
@@ -10,4 +15,10 @@ public class SpringBootRedisSampleApplication {
         SpringApplication.run(SpringBootRedisSampleApplication.class, args);
     }
 
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 }
