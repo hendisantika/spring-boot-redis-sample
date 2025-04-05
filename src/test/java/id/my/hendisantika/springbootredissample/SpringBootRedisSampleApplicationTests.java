83f68259248daa0c857094eb25a2b2dc6efc87db
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 07:35:39 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springbootredissample/SpringBootRedisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootredissample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
