6aa685970f09278c2a159d24bfb67f0ba4e086ee
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 07:45:31 +0700

    feat: Add CreateBooks🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootredissample/boot/CreateBooks.java
+++ src/main/java/id/my/hendisantika/springbootredissample/boot/CreateBooks.java
@@ -1,5 +1,9 @@
 package id.my.hendisantika.springbootredissample.boot;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.springbootredissample.model.Book;
+import id.my.hendisantika.springbootredissample.model.Category;
 import id.my.hendisantika.springbootredissample.repository.BookRepository;
 import id.my.hendisantika.springbootredissample.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +11,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +43,51 @@ public class CreateBooks implements CommandLineRunner {
     private final BookRepository bookRepository;
 
     private final CategoryRepository categoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (bookRepository.count() == 0) {
+            ObjectMapper mapper = new ObjectMapper();
+            TypeReference<List<Book>> typeReference = new TypeReference<List<Book>>() {
+            };
+
+            List<File> files = //
+                    Files.list(Paths.get(getClass().getResource("/data/books").toURI())) //
+                            .filter(Files::isRegularFile) //
+                            .filter(path -> path.toString().endsWith(".json")) //
+                            .map(java.nio.file.Path::toFile) //
+                            .collect(Collectors.toList());
+
+            Map<String, Category> categories = new HashMap<>();
+
+            files.forEach(file -> {
+                try {
+                    log.info(">>>> Processing Book File: {}", file.getPath());
+                    String categoryName = file.getName().substring(0, file.getName().lastIndexOf("_"));
+                    log.info(">>>> Category: {}", categoryName);
+
+                    Category category;
+                    if (!categories.containsKey(categoryName)) {
+                        category = Category.builder().name(categoryName).build();
+                        categoryRepository.save(category);
+                        categories.put(categoryName, category);
+                    } else {
+                        category = categories.get(categoryName);
+                    }
+
+                    InputStream inputStream = new FileInputStream(file);
+                    List<Book> books = mapper.readValue(inputStream, typeReference);
+                    books.stream().forEach((book) -> {
+                        book.addCategory(category);
+                        bookRepository.save(book);
+                    });
+                    log.info(">>>> {} Books Saved!", books.size());
+                } catch (IOException e) {
+                    log.info("Unable to import books: {}", e.getMessage());
+                }
+            });
+
+            log.info(">>>> Loaded Book Data and Created books...");
+        }
+    }
 }
