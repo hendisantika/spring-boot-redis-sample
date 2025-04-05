424266ec83c53847b4e067adb7eae8e933b3c7b1
hendisantika <hendisantika@yahoo.co.id>
2025-04-05 07:40:46 +0700

    feat: Add User Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootredissample/model/Role.java
@@ -0,0 +1,48 @@
+package id.my.hendisantika.springbootredissample.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-redis-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 05/04/25
+ * Time: 07.38
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Represents a role entity stored in Redis.
+ *
+ * <p>This class is annotated with {@link Builder} to provide a builder pattern for creating instances, and
+ * {@link Data} to generate getters, setters, equals, hashCode, and toString methods. The class is also annotated
+ * with {@link RedisHash} to indicate that it is a Redis hash stored in Redis.</p>
+ */
+@Data
+@Builder
+@RedisHash
+public class Role {
+
+    /**
+     * The unique identifier for the role.
+     *
+     * <p>This field is marked with {@link Id} to indicate it is the primary key in Redis.</p>
+     */
+    @Id
+    private String id;
+
+    /**
+     * The name of the role.
+     *
+     * <p>This field is indexed to allow for efficient querying by role name.</p>
+     */
+    @Indexed
+    private String name;
+}
